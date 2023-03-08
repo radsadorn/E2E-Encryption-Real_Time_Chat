41ec9e28553a0a8de4d6a5f9c129140b5a71039f
Pakorn Thanaprachanon <Pakorn.Thanaprachanon@outlook.com>
2023-03-08 14:27:53 +0700

    Update code

--- server/src/modules/authorization/auth.controller.ts
+++ server/src/modules/authorization/auth.controller.ts
@@ -3,59 +3,58 @@ import ResponseUtil from '../../shared/response.util';
 import { AuthorizationService } from './auth.service';
 
 export class AuthorizationController {
+  private readonly LOG_NAME = 'AuthorizationController\t\t> ';
 
-    private readonly LOG_NAME = 'AuthorizationController\t\t> ';
+  constructor(
+    private authService: AuthorizationService = new AuthorizationService()
+  ) {}
 
-    constructor (
-        private authService: AuthorizationService = new AuthorizationService(),
-    ) {}
+  public async authorization(req: Request, res: Response): Promise<void> {
+    try {
+      console.log(this.LOG_NAME + 'Authorization');
 
-    public async authorization(req: Request, res: Response): Promise<void> {
-        try {
-            console.log(this.LOG_NAME + 'Authorization');
-            
-            res.json(ResponseUtil.getResponseSuccessWithResult({ message: 'Hello' }));
-        } catch (err: any) {
-            this.getErrorResponse(err, res);
-        }
+      res.json(ResponseUtil.getResponseSuccessWithResult({ message: 'Hello' }));
+    } catch (err: any) {
+      this.getErrorResponse(err, res);
     }
+  }
 
-    public async createNewUser(req: Request, res: Response): Promise<void> {
-        try {
-            console.log(this.LOG_NAME + 'New User...');
+  public async createNewUser(req: Request, res: Response): Promise<void> {
+    try {
+      console.log(this.LOG_NAME + 'New User...');
 
-            const result = await this.authService.createNewUser(req.body);
-            res.json(ResponseUtil.getResponseSuccess());
-        } catch (err: any) {
-            this.getErrorResponse(err, res);
-        }
+      const result = await this.authService.createNewUser(req.body);
+      res.json(ResponseUtil.getResponseSuccess());
+    } catch (err: any) {
+      this.getErrorResponse(err, res);
     }
+  }
 
-    public async userLogin(req: Request, res: Response): Promise<void> {
-        try {
-            console.log(this.LOG_NAME + 'User Login...');
+  public async userLogin(req: Request, res: Response): Promise<void> {
+    try {
+      console.log(this.LOG_NAME + 'User Login...');
 
-            const token = await this.authService.login(req.body);
-            console.log(token);
-            res.header('Authorization', token);
-            res.json(ResponseUtil.getResponseSuccessWithResult({ token }));
-        } catch (err: any) {
-            this.getErrorResponse(err, res);
-        }
+      const token = await this.authService.login(req.body);
+      console.log(token);
+      res.header('Authorization', token);
+      res.json(ResponseUtil.getResponseSuccessWithResult({ token }));
+    } catch (err: any) {
+      this.getErrorResponse(err, res);
     }
+  }
 
-    public async userLogout(req: Request, res: Response): Promise<void> {
-        try {
-            console.log(this.LOG_NAME + 'User Logout...');
+  public async userLogout(req: Request, res: Response): Promise<void> {
+    try {
+      console.log(this.LOG_NAME + 'User Logout...');
 
-            res.json(ResponseUtil.getResponseSuccess());
-        } catch (err: any) {
-            this.getErrorResponse(err, res);
-        }
+      res.json(ResponseUtil.getResponseSuccess());
+    } catch (err: any) {
+      this.getErrorResponse(err, res);
     }
+  }
 
-    private getErrorResponse(error: any, res: Response): void {
-        console.log('ERROR: ' + this.LOG_NAME + JSON.stringify(error));
-        res.json(ResponseUtil.getResponseError(error.message));
-    }
+  private getErrorResponse(error: any, res: Response): void {
+    console.log('ERROR: ' + this.LOG_NAME + JSON.stringify(error));
+    res.json(ResponseUtil.getResponseError(error.message));
+  }
 }
--- server/src/modules/authorization/auth.service.ts
+++ server/src/modules/authorization/auth.service.ts
@@ -1,77 +1,74 @@
-import { NewUser, UserData, UserLogin, UserResponse } from "./auth.interface";
-import { AuthorizationRepository } from "./auth.repository";
+import { NewUser, UserData, UserLogin, UserResponse } from './auth.interface';
+import { AuthorizationRepository } from './auth.repository';
 import { hash, compare } from 'bcrypt';
-import { generateToken } from "../../shared/jwt.util";
+import { generateToken } from '../../shared/jwt.util';
 
 const saltRounds = 10;
 
 export class AuthorizationService {
+  private readonly LOG_NAME = 'AuthorizationService\t\t> ';
 
-    private readonly LOG_NAME = 'AuthorizationService\t\t> ';
+  users: { [key: string]: NewUser } = {};
 
-    users: { [key: string]: NewUser } = {};
+  constructor(
+    private authRepo: AuthorizationRepository = new AuthorizationRepository()
+  ) {}
 
-    constructor (
-        private authRepo: AuthorizationRepository = new AuthorizationRepository(),
-    ) {}
+  public async createNewUser(newUser: NewUser): Promise<string> {
+    try {
+      console.log(
+        this.LOG_NAME + 'Create New User : ' + JSON.stringify(newUser)
+      );
 
-    public async createNewUser(newUser: NewUser): Promise<string> {
-        try {
-            console.log(this.LOG_NAME + JSON.stringify(newUser));
+      // find by username
+      // username collision
+      const result = this.users[newUser.username];
+      if (result) throw { message: 'Username is already exist!!!' };
 
-            // find by username
-            // username collision
-            const result = this.users[newUser.username];
-            if (result)
-                throw { message: "Username is already exist!!!" };
+      const hashPassword: string = await hash(newUser.password, saltRounds);
+      const userData: NewUser = {
+        ...newUser,
+        password: hashPassword,
+      };
 
-            const hashPassword: string = await hash(newUser.password, saltRounds)
-            const userData: NewUser = {
-                ...newUser,
-                password: hashPassword,
-            }
+      // this.authRepo.createNewUser(userData);
 
-            // this.authRepo.createNewUser(userData);
+      this.users[newUser.username] = userData;
 
-            this.users[newUser.username] = userData;
+      console.log(this.users);
 
-            console.log(this.users);
-
-            return generateToken(userData);
-            //return user data
-        } catch (err: any) {
-            throw err;
-        }
+      return generateToken(userData);
+      //return user data
+    } catch (err: any) {
+      throw err;
     }
+  }
 
-    public async login(userLogin: NewUser): Promise<string> {
-        try {
-            console.log(this.LOG_NAME + JSON.stringify(userLogin));
+  public async login(userLogin: NewUser): Promise<string> {
+    try {
+      console.log(this.LOG_NAME + 'User login : ' + JSON.stringify(userLogin));
 
-            // find by username
-            const result = this.users[userLogin.username];
-            if (!result)
-                return await this.createNewUser(userLogin);
+      // find by username
+      const result = this.users[userLogin.username];
+      if (!result) return await this.createNewUser(userLogin);
 
-            const password = result.password;
+      const password = result.password;
 
-            const userData: UserResponse = {
-                username: result.username,
-                publicKeyBase64: result.publicKeyBase64,
-                privateKeyBase64: result.privateKeyBase64,
-            }
+      const userData: UserResponse = {
+        username: result.username,
+        publicKeyBase64: result.publicKeyBase64,
+        privateKeyBase64: result.privateKeyBase64,
+      };
 
-            const passwordCorrected = await compare(userLogin.password, password);
+      const passwordCorrected = await compare(userLogin.password, password);
 
-            if (passwordCorrected) {
-                return generateToken(userData);
-            } 
+      if (passwordCorrected) {
+        return generateToken(userData);
+      }
 
-            throw new Error("Password Incorrect!!");
-
-        } catch (err: any) {
-            throw err;
-        }
+      throw new Error('Password Incorrect!!');
+    } catch (err: any) {
+      throw err;
     }
-
+  }
 }
